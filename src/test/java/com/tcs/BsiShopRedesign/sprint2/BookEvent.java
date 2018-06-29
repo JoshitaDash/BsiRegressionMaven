6599de6d1227617001f81597d9da32a63cebfe46
JoshitaDash <dash.joshita@tcs.com>
2018-06-29 09:16:01 +0000

    free event commented

--- src/test/java/com/tcs/BsiShopRedesign/sprint2/BookEvent.java
+++ src/test/java/com/tcs/BsiShopRedesign/sprint2/BookEvent.java
@@ -86,7 +86,7 @@ public class BookEvent extends BaseTest {
 		}
 	}
 
-	@Test(priority = 3, enabled = true)
+	@Test(priority = 3, enabled = false)
 	public void BookFreeEvent() {
 
 		test = extent.startTest("Sprint 2 - Book Free Event");
